a4f38ab0358feb092b9a081d7cac5eabfbab764c
keen1 <ke3n1@proton.me>
2022-12-10 01:50:07 -0500

    my testmatrices class works it seems, so i am implementing the bit shifts and xors correctly. need to test in full crpyo scheme.

--- src/TestMatrices.java
+++ src/TestMatrices.java
@@ -24,24 +24,64 @@ public class TestMatrices {
         testBlock(state);
 
         //perform the mix_Columns operation
-
+        state = mixColumns(matrixFactor, state);
+        //print the result
+        testBlock(state);
+        //perform the inverse mix columns
+        state = invMixColumns(invMatrixFactor, state);
+        //print the result
+        testBlock(state);
 
 
 
     }
-    public static void mixColumns(int[][] matrixFactor, int[][] stateBlock){
-        int[] stateColumn = new int[4];
+    public static int[][] mixColumns(int[][] matrixFactor, int[][] stateBlock){
+        int[][] mixedBlock = new int[4][4];
         for(int i = 0; i < stateBlock.length; i++){
             int[] scalarRow = matrixFactor[i];
             for(int k = 0; k < stateBlock.length; k++){
-                stateColumn = {};
+                int[] stateColumn = {stateBlock[0][k], stateBlock[1][k], stateBlock[2][k], stateBlock[3][k]};
+                mixedBlock[i][k] = gfMultOp(scalarRow, stateColumn);
             }
         }
+        return mixedBlock;
     }
-    public static void invMixColumns(int[][] invMatrixFactor, int[][] stateBlock){
+    public static int[][] invMixColumns(int[][] invMatrixFactor, int[][] stateBlock){
+        int[][] mixedBlock = new int[4][4];
 
+        for(int i = 0; i < stateBlock.length;i++){
+            int[] scalarRow = invMatrixFactor[i];
+            for(int k = 0; k < stateBlock[i].length; k++){
+                int[] stateColumn = {stateBlock[0][k], stateBlock[1][k], stateBlock[2][k], stateBlock[3][k]};
+                mixedBlock[i][k] = gfMultOp(scalarRow, stateColumn);
+
+            }
+        }
+        return mixedBlock;
     }
-    public static void gfMultOp(int[] scalarRow, int[] stateColumn){
+    public static int gfMultOp(int[] scalarRow, int[] stateColumn){
+        for(int i = 0; i < scalarRow.length; i++){
+            if(scalarRow[i] == 2){
+                stateColumn[i] = modPrimePoly(stateColumn[i] << 1);
+            }
+            if(scalarRow[i] == 3){
+                stateColumn[i] = modPrimePoly((stateColumn[i] << 1)^stateColumn[i]);
+            }
+            if(scalarRow[i] == 9){
+                stateColumn[i] = modPrimePoly((stateColumn[i] << 3)^stateColumn[i]);
+            }
+            if(scalarRow[i] == 11){
+                stateColumn[i] = modPrimePoly((stateColumn[i] << 3)^(stateColumn[i] << 1)^stateColumn[i]);
+
+            }
+            if(scalarRow[i] == 13){
+                stateColumn[i] = modPrimePoly((stateColumn[i] <<3)^(stateColumn[i] << 2)^stateColumn[i]);
+            }
+            if(scalarRow[i] == 14){
+                stateColumn[i] = modPrimePoly((stateColumn[i] <<3 )^(stateColumn[i] << 2)^(stateColumn[i]<<1));
+            }
+        }
+        return stateColumn[0]^stateColumn[1]^stateColumn[2]^stateColumn[3];
 
     }
     public static int modPrimePoly(int value){
@@ -57,8 +97,10 @@ public class TestMatrices {
         for(int i = 0; i < block.length; i++){
             System.out.println();
             for(int k = 0; k < block[i].length; k++){
-                System.out.print(block[i][k] + " ");
+                String bStr = Integer.toHexString(block[i][k]);
+                System.out.print(bStr + " ");
             }
         }
+        System.out.println("\n________________________________________________________________");
     }
 }
